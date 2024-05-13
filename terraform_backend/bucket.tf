23e3d24dac4d1c15d11a8ee7c256c90d2e983f4a
liorl <liorl@clouderide.co.il>
2024-05-13 10:32:40 +0300

    add terraform backend folder for creating a bucket for the state file

---
+++ terraform_backend/bucket.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+}
+
+##### creating s3 bucket and dynamodb #####
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "flight-tlv-state-file"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket        = aws_s3_bucket.terraform_state.bucket 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "flight-tlv-state-locking-file"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ terraform_backend/variables.tf
@@ -0,0 +1,11 @@
+variable "profile" {
+  type = string
+  default = "default"
+  description = "configuration and credential profile on local machine"
+}
+
+variable "region" {
+  type = string
+  default = "eu-central-1"
+  description = "AWS region"
+}
