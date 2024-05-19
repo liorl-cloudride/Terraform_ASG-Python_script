7d5e3a115f4f20b096c9653691f402e8d351ad61
liorl <liorl@clouderide.co.il>
2024-05-19 15:07:43 +0300

    changed script file name and deleted the old one.

--- script/ec2_list.py
+++ script/ec2_list.py
@@ -1,61 +1,53 @@
-# list of machines (ec2) that are up
-# region | instance type | external IP (Public)
-
 import boto3
+from botocore.exceptions import ClientError
+import datetime
 
-def list_ec2_instances() -> None:
+def list_ec2_instances():
     session = boto3.Session(profile_name='flight-tlv')
 
-    # ec2_client = session.client('ec2')
-    # print(ec2_client.describe_regions()['Regions'])
-    # regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
-    # print(regions)
-    with open('regions.txt', 'r') as file:
-        regions = [line.strip() for line in file.readlines()]
-    # try:
-    #     with open('regions.txt', 'r') as file:
-    #         regions = [line.strip() for line in file if line.strip()]  # Ensure we ignore empty lines
-    #     # If the file is empty, fetch all regions
-    #     if not regions:
-    #         ec2_client = session.client('ec2')
-    #         regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
-    #         print(regions)
-    # except FileNotFoundError:
-    #     # If the file does not exist, fetch all regions
-    #     print("regions.txt file not found. Using all available regions.")
-    #     ec2_client = session.client('ec2')
-    #     regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
-    #     print(regions)
-        
+    try:
+        with open('regions.txt', 'r') as file:
+            regions = [line.strip() for line in file if line.strip()]
+    except FileNotFoundError:
+        print("regions.txt file not found. Querying all regions where opt-in is not required.")
+        regions = None
+
+    if not regions:
+        print("regions.txt file is empty. Querying all regions where opt-in is not required.")
+        ec2_client = session.client('ec2')
+        response = ec2_client.describe_regions(Filters=[
+            {'Name': 'opt-in-status', 'Values': ['opt-in-not-required']}
+        ])
+        regions = [region['RegionName'] for region in response['Regions']]
+        print("Regions to be queried:", regions)
+
     all_instances = []
 
     for region in regions:
         print(f"Checking EC2 instances in region: {region}")
         region_client = session.client('ec2', region_name=region)
-        print("check")
-        response = region_client.describe_instances(
-            Filters=[
-                {
-                    'Name': 'instance-state-name',
-                    'Values': [
-                        'stopped'
-                    ]
-                }
-            ]
-        )
-        print("check2")
 
-        # print(response)
+        try:
+            response = region_client.describe_instances(
+                Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}]
+            )
 
-        for reservation in response['Reservations']:
-            for instance in reservation['Instances']:
-                instance_details = {
-                    'Region': region,
-                    'Instance Type': instance.get('InstanceType'),
-                    'External IP (Public)': instance.get('PublicIpAddress', "None")
-                }
-                all_instances.append(instance_details)
-                # print(all_instances)
+            for reservation in response['Reservations']:
+                for instance in reservation['Instances']:
+                    instance_details = {
+                        'Region': region,
+                        'Instance Type': instance.get('InstanceType'),
+                        'External IP (Public)': instance.get('PublicIpAddress', "None")
+                    }
+                    all_instances.append(instance_details)
+        except ClientError as error:
+            print(f"Failed to retrieve instances for {region}. Error: {error}")
+            if error.response['Error']['Code'] == "AuthFailure":
+                print("Authentication failure. Possible issues with access in", region)
+                try:
+                    print("Token details: Expiry:", session.get_credentials().expiration)
+                except AttributeError:
+                    print("No expiration information available for credentials.")
 
     print("\nList of running EC2 instances:")
     print("Region | Instance Type | External IP")
